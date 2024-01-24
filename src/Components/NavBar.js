fe789af2d8ab40b1666304127f79582cdda19ab0
Abhishek Kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2024-01-24 13:52:23 +0530

    a

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -9,7 +9,7 @@ export default function NavBar() {
         <a class="navbar-brand" href="/">
         <img src="./logo2.png" alt="Logo" width="40" height="40" class="d-inline-block align-text-top"/></a>
         <a className="navbar-brand mb-0 h1" href="/">Sophia 1.0</a>     
-        <a className="navbar-brand" href="/">Learnings</a>
+        <a className="navbar-brand" href=""></a>
         </div>
       </nav>
     </div>
